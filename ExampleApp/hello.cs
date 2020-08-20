95b986a6177b8712e956ebdf0a7fed9b8791bc65
Lukas Gužauskas <lgu@fabricair.com>
2020-08-20 17:58:55 +0300

    add hello class

---
+++ ExampleApp/hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    public class hello
+    {
+        public string Hello = "World!";
+    }
+}
